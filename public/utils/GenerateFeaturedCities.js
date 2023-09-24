d3e5c908dd22b18c9cd6d0b48e396108650d483a
corentinesteve <corentinesteve@yahoo.fr>
2023-09-24 15:36:39 +0200

    featured cities card updated

--- public/utils/GenerateFeaturedCities.js
+++ public/utils/GenerateFeaturedCities.js
@@ -1,8 +1,9 @@
 import { sortCities } from './SortCities.js';
 import { capitalizeFirstLetterOfEachWord } from './CapitalizeFirstLetterOfEachWord.js';
 import { navigateToCity } from './NavigateToCity.js';
+import { convertToCommuneFormat } from './ConvertToCommuneFormat.js';
 
-export function generateFeaturedCities(newDataMap) {
+export function generateFeaturedCities(newDataMap, communesMap) {
     const allCityLabels = Array.from(newDataMap.keys());
     const sortedCityLabels = sortCities(allCityLabels, 'scoreDesc', newDataMap).slice(0, 3);  // Take top 3
 
@@ -12,6 +13,9 @@ export function generateFeaturedCities(newDataMap) {
     featuredDiv.appendChild(featuredCitiesDiv);
 
     sortedCityLabels.forEach((cityLabel) => {
+        const convertedLabel = convertToCommuneFormat(cityLabel);
+        const communeData = communesMap.get(convertedLabel);
+
         const cityData = newDataMap.get(cityLabel);
         let score = cityData?.score;
         let displayScore = !isNaN(parseFloat(score)) ? `${score}` : '-';
@@ -23,6 +27,7 @@ export function generateFeaturedCities(newDataMap) {
         card.innerHTML = `
             <div class="card-header">
                 <h2>${capitalizedCityLabel}</h2>
+                <h3>${communeData.nom_departement} (${communeData.code_departement})</h3>
                 <p>Score: ${displayScore}</p>
             </div>
             <i class="fas fa-arrow-right" aria-hidden="true"></i>
